4956fe8273bb658cf4a14503a21b6d719505543d
Victor Ro <vctr.ro@gmail.com>
2020-02-05 16:54:43 +0400

    changed arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -54,13 +54,24 @@ namespace Arrays
             int[,,] arr30 = new int[3, 4, 5]; // трёхмерный массив
 
             int[][] arr40 = new int[5][]; // зубчатый массив, массив из массивов разной длины
-            arr40[0] = arr10;
+            int[][] arr41 =               // объявление с инициализацией
+            {
+                new int[]{1, 3, 5},
+                new int[3],
+                new []{1, 3, 5}
+
+            };
+
+            arr40[0] = arr10; // массив arr40[0] ссылается на туже область памяти, что и arr10
+            arr40[0][0] = 15; // изменения значений одного ведут к изменениям в другом
+            arr40[0][3] = 12;
+            Console.WriteLine($"{String.Join(", ", arr10)}");
+
             arr40[1] = new int[] { 10, 5, 7, 15 };
             arr40[2] = new int[arr13.Length];
             Array.Copy(arr13, arr40[2], arr13.Length);
-            arr40[3] = new int[arr14.Length*2];
+            arr40[3] = new int[arr14.Length * 2];
             Array.Copy(arr14, 0, arr40[3], 4, arr14.Length);
-
             Console.WriteLine($"{String.Join(", ", arr40[3])}");
         }
     }
---
+++ Learning/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 2 * 3 + 2;
+            Console.WriteLine("Hello World!" + i);
+        }
+    }
+}
